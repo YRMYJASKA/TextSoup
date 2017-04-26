07c68476e0126aa98dbb9390f38777c788748482
YRMYJASKA <hjelt.jyry@gmail.com>
2017-04-26 18:35:41 +0000

    Nothing too much important going on here

--- src/main.c
+++ src/main.c
@@ -7,7 +7,9 @@ int mainLoop();
 // Important "constants"
 int MAX_X, MAX_Y;
 int CURS_X, CURS_Y;
-
+int running = 1;
+char key;
+FILE* ofile;
 int main(int argc, char *argv[]){
 	
 	// Initializing the curses session
@@ -15,11 +17,20 @@ int main(int argc, char *argv[]){
 	raw();
 	keypad(stdscr, TRUE);
 	noecho();
-
+	curs_set(TRUE);
+	// Main loop
+	while(running){	
+	refresh();
+	getmaxyx(stdscr, MAX_Y, MAX_X);
+	mvprintw(0,0,"%i", MAX_X); 	
+	key = getch();
+	if(key == KEY_F(1))
+		running = 0;
+	}
 	// End curses session
+	getch(); //Debug
 	endwin();
 	return 0;
 }
 int mainLoop(){
-	return 0;
 }
